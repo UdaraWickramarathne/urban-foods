8c8bd750e1aa5d36af02a0b97b3304aee0bee223
technoidteam <sahandulmith@gmail.com>
2025-03-21 00:55:16 +0530

    update login and register form style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,52 @@
-import './App.css'
+import './App.css';
 import React, { useState } from 'react';
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Homepage from './pages/Homepage/Homepage'
-import LoginPage from './components/AuthUi/Login'
+import Navbar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage/Homepage';
+import LoginPage from './components/AuthUi/Login';
+import Register from './components/AuthUi/Register';
 
 function App() {
-
   const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
 
   const handleUserIconClick = () => {
-    setShowLogin(!showLogin);
+    setShowLogin(true);
+    setShowRegister(false);
   };
+
   const handleCloseLogin = () => {
     setShowLogin(false);
   };
 
+  const handleSwitchToRegister = () => {
+    setShowLogin(false);
+    setShowRegister(true);
+  };
+
+  const handleCloseRegister = () => {
+    setShowRegister(false);
+  };
+
+  const handleSwitchToLogin = () => {
+    setShowRegister(false);
+    setShowLogin(true);
+  };
 
   return (
-    <>  
-      <Navbar onUserIconClick={handleUserIconClick}/>
-      {showLogin && <LoginPage  onClose={handleCloseLogin} />}
+    <>
+      <Navbar onUserIconClick={handleUserIconClick} />
+      {showLogin && (
+        <LoginPage onClose={handleCloseLogin} onSwitchToRegister={handleSwitchToRegister} />
+      )}
+      {showRegister && (
+        <Register onClose={handleCloseRegister} onSwitchToLogin={handleSwitchToLogin} />
+      )}
       <Routes>
-        <Route path="/" element={<Homepage/>}/>
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
