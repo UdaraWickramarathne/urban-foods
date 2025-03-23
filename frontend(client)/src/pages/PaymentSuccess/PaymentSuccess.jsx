1a4f8d53d16086f94ecb7c2e8d73d8ed71156287
technoidteam <sahandulmith@gmail.com>
2025-03-24 01:53:39 +0530

    cart screen update

--- frontend(client)/src/pages/PaymentSuccess/PaymentSuccess.jsx
+++ frontend(client)/src/pages/PaymentSuccess/PaymentSuccess.jsx
@@ -2,64 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./PaymentSuccess.css";
 import animationData from "../../assets/success.json";
 import Lottie from "lottie-react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { useContext } from "react";
-import { StoreContext } from "../../context/StoreContext";
+
 
 const PaymentSuccess = () => {
-  const navigate = useNavigate();
-  const orderData = JSON.parse(localStorage.getItem("orderData")) || {};
-  const [details, setDetails] = useState(orderData);
-  const { token, url, setCartDetails, setOrderDetails } =
-    useContext(StoreContext);
-
-  useEffect(() => {
-    console.log("Order data:", details);
-
-    const saveOrderData = async () => {
-      try {
-        const response = await axios.post(`${url}/orders`, orderData, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        if (response.status === 200) {
-          localStorage.removeItem("orderData");
-          setCartDetails({
-            itemCount: 0,
-            totalAmount: 0.0,
-          });
-          setOrderDetails([]);
-        } else {
-          alert("Order failed!");
-        }
-      } catch (error) {
-        console.error("Error saving order data:", error);
-        alert("An error occurred while saving order data.");
-      }
-    };
-    saveOrderData();
-  }, []);
-
-  function formatDateToUS(isoDateString) {
-    if (!isoDateString) return "N/A"; // Handle missing date
-    const date = new Date(isoDateString);
-
-    // Add validation for Date object
-    if (isNaN(date)) {
-      console.error("Invalid date string:", isoDateString);
-      return "Invalid Date";
-    }
-
-    const options = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      timeZone: "UTC",
-    };
-    return date.toLocaleDateString("en-US", options);
-  }
 
   return (
     <div className="success-container">
--- frontend(client)/vite.config.js
+++ frontend(client)/vite.config.js
@@ -18,4 +18,7 @@ export default defineConfig({
       ],
     },
   },
+  define: {
+    'process.env': {}, // Define process.env to avoid the error
+  },
 })
